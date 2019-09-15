70186b37cef7b9490e7fa8ad2eca81e4b06ef2f7
ManuRafa6 <daniniviru@gmail.com>
2019-09-15 22:07:52 +0200

    vaca mejorada

--- Ejercicios/vaca.py
+++ Ejercicios/vaca.py
@@ -1,8 +1,8 @@
 frase_a_convertir = input("Introduce la frase que quieres convertir: ")
-vocales = ["a", "e", "i", "o", "u"]
+aes = ["a", "A"]
 
 for i in frase_a_convertir:
-    if i == "A" or i == "a":
+    if i in aes:
         frase_a_convertir = frase_a_convertir.replace(i, "VACA", 1)
 
 print(frase_a_convertir)
