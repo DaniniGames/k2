6b6dc34cee89e9a2e1a97e828f56c6e2067a14c3
ManuRafa6 <daniniviru@gmail.com>
2019-09-15 21:55:25 +0200

    ejercicios de Sr. Pous

--- Ejercicios/vocales_a_i.py
+++ Ejercicios/vocales_a_i.py
@@ -1,8 +1,11 @@
 frase_a_cambiar = input("Introduce la frase a modificar: ").lower()
-vocales = ["a", "e", "i", "o", "u"]
+vocales = ["a", "e", "o", "u",]
+tildes = ["á", "é", "ó", "ú"]
 
 for i in frase_a_cambiar:
     if i in vocales:
-        frase_a_cambiar = frase_a_cambiar.replace(i, "i", 1)
+        frase_a_cambiar = frase_a_cambiar.replace(i, "i", 10)
+    elif i in tildes:
+        frase_a_cambiar = frase_a_cambiar.replace(i, "í", 10)
 
 print(frase_a_cambiar)
