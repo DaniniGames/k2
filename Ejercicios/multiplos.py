af03c9d3c6604f360ded7cd696930c1c4e3ee190
ManuRafa6 <daniniviru@gmail.com>
2019-09-16 21:01:12 +0200

    Multiplos ya perfeccionado

--- Ejercicios/multiplos.py
+++ Ejercicios/multiplos.py
@@ -1,4 +1,9 @@
-lista = [2, 4, 7, 70, 80, 23, 54]
+input_usuario = int(input("Introduce los números uno a uno. Escribe 0 para analizarlos: "))
+lista = []
+
+while input_usuario != 0:
+    lista.append(input_usuario)
+    input_usuario = int(input("Introduce los números uno a uno. Escribe 0 para analizarlos: "))
 
 multiplos_dos = []
 multiplos_tres = []
@@ -15,7 +20,7 @@ for i in lista:
     if i % 7 == 0:
         multiplos_siete.append(i)
 
-print("Múltiplos de 2: {}".format(multiplos_dos))
+print("Múltiplos de 2: {} ".format(multiplos_dos))
 print("Múltiplos de 3: {}".format(multiplos_tres))
 print("Múltiplos de 5: {}".format(multiplos_cinco))
 print("Múltiplos de 7: {}".format(multiplos_siete))
